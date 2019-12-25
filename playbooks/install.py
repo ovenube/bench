cc52e44fb9e0e30ce3ce6d36b69b4dc082fc81a9
seethersan <carlos_jcez@hotmail.com>
2019-12-25 13:38:30 -0500

    update bench to version-12

--- playbooks/install.py
+++ playbooks/install.py
@@ -357,13 +357,13 @@ def parse_commandline_args():
 
 	parser.add_argument('--repo-url', dest='repo_url', help='Clone bench from the given url')
 
-	parser.add_argument('--frappe-repo-url', dest='frappe_repo_url', action='store', default='https://github.com/frappe/frappe',
+	parser.add_argument('--frappe-repo-url', dest='frappe_repo_url', action='store', default='https://github.com/ovenube/frappe',
 		help='Clone frappe from the given url')
 
 	parser.add_argument('--frappe-branch', dest='frappe_branch', action='store',
 		help='Clone a particular branch of frappe')
 
-	parser.add_argument('--erpnext-repo-url', dest='erpnext_repo_url', action='store', default='https://github.com/frappe/erpnext',
+	parser.add_argument('--erpnext-repo-url', dest='erpnext_repo_url', action='store', default='https://github.com/ovenube/erpnext',
 		help='Clone erpnext from the given url')
 
 	parser.add_argument('--erpnext-branch', dest='erpnext_branch', action='store',
